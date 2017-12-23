e25c9a0f85b6220f0cf542cc38a8c01f0ef898a4
luohao <553569682@qq.com>
2017-12-23 10:44:00 +0800

    my

--- src/main/resources/static/js/detail.js
+++ src/main/resources/static/js/detail.js
@@ -37,7 +37,35 @@
 
     $("#search").on("change", function(e){
         var nickName = $(e.target).val();
-        alert(nickName)
+        if(nickName) {
+            $.ajax({
+                url: urlPrefix + "hall/users/" + hall.id + "/" + nickName,
+                type: 'get',
+                dataType: 'json',
+                success: function (res) {
+                    if (res && res.length > 0) {
+                        var users = '';
+                        res.forEach(function (item, index) {
+                            if (index <= 10) {
+                                users += '<li> \
+                        <label class="label-checkbox item-content" user-id="' + item.id + '"> \
+                        <input type="radio" name="my-radio" /> \
+                        <div class="item-media"><i class="icon icon-form-checkbox"></i></div> \
+                        <div class="item-inner"> \
+                        <div class="item-title-row"> \
+                        <div class="item-title">' + item.wxnickname + '</div> \
+                        </div> \
+                        </div> \
+                        </label> \
+                        </li>';
+                            }
+                        });
+
+                        $('#users').empty().append(users);
+                    }
+                }
+            });
+        }
     });
 
     var curAddr = {};
